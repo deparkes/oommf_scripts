038e7ec20c677c61c27cfa5cc4f28e75809c3cb8
unknown <ppxdep@pppzawr01.ad.nottingham.ac.uk>
2012-02-26 12:32:30 +0000

    Added plans for time outputs and modifiable batch list. Made file output time before and after running boxsi. Made a test list structures.txt
    Signed-off-by: unknown <ppxdep@pppzawr01.ad.nottingham.ac.uk>

--- oommf_script.py
+++ oommf_script.py
@@ -5,6 +5,7 @@
 def main():
     import os
     import subprocess
+    import time
     path_tcl = 'C:/Tcl/bin/tclsh84'
     path_boxsi_base = 'C:/oommf/oommf.tcl boxsi -exitondone 1 -parameters'
     path_mif_file = './FeGa_Bar_Field_1.mif'
@@ -14,6 +15,8 @@ def main():
     #os.system(oommf_string)
 ##    img_list = ['Cross_4um.bmp','Lbar_2um.bmp','BarPad_7um_1um.bmp', 'Ring_10um_2um.bmp']
     img_list = ['Square_5um.bmp','Ring_10um_2um.bmp','BarPad_3um_1um.bmp','BarPad_5um_1um.bmp','BarPad_6um_1um.bmp','BarPad_7um_1um.bmp']
+#    I want to allow the script to read in a list of image files to simulate.
+#I want the script to read in the file eaach time so that I can update my batch job on the fly.
 ##    img_list = ['Lbar_2um.bmp']
     strain_list = ['0']
     # loop through the different devices
@@ -23,16 +26,22 @@ def main():
 ##        set the output directory name. if this directory doesn't
 ##        exist make it. This means that we can have an automatic
 ##        output folder in the mif file.
-        img_dir = '../output/%s' % (img_count)
-        if not os.path.exists(img_dir):
-            os.makedirs(img_dir)
+##        img_dir = '../output/%s' % (img_count)
+##        if not os.path.exists(img_dir):
+##            os.makedirs(img_dir)
 
         for strain_count in strain_list:
             path_boxsi = path_boxsi_base + ' \"Ks %s img %s \"' % (strain_count,img_count)
             oommf_string = "%s %s %s" % (path_tcl, path_boxsi,path_mif_file)
             print (' %s \n') % (oommf_string)
-            subprocess.call(oommf_string)
-            
+            localtime = time.asctime( time.localtime(time.time()) )
+            print "Start time :", localtime
+##                      subprocess.call(oommf_string)
+            localtime = time.asctime( time.localtime(time.time()) )
+            print "End time :", localtime,"\n"
+#            I want the script to output the start and end time.
+
+
 
 if __name__ == "__main__":
     main()
